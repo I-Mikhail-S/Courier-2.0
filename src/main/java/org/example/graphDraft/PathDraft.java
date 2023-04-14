162b6b064453ae1a65af396a870192542677d5b2
ThatPotato <gamelikecar@gmail.com>
2023-04-14 13:41:06 +0400

    Draft

---
+++ src/main/java/org/example/graphDraft/PathDraft.java
@@ -0,0 +1,32 @@
+package org.example.graphDraft;
+
+
+import java.util.ArrayList;
+import java.util.List;
+/*
+public class PathDraft { // объект данного класса содержащий расстояние и предыдущие и пройденные вершины
+    private int distance; // текущая дистанция от начальной вершины
+    private List<Integer> parentVertices; // текущий родитель вершины
+
+    public PathDraft(int distance) {
+        this.distance = distance;
+        this.parentVertices = new ArrayList<>();
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public List<Integer> getParentVertices() {
+        return parentVertices;
+    }
+
+    public void setParentVertices(List<Integer> parentVertices) {
+        this.parentVertices = parentVertices;
+    }
+}
+*/
