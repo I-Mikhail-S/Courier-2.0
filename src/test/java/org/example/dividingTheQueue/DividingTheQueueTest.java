ac8c2f780d987f708cd9e376d6db891086294562
Mikhail <michailivanchin@yandex.ru>
2023-05-10 19:42:28 +0300

    noo

--- src/test/java/org/example/dividingTheQueue/DividingTheQueueTest.java
+++ src/test/java/org/example/dividingTheQueue/DividingTheQueueTest.java
@@ -32,16 +32,16 @@ class DividingTheQueueTest {
         List<Order> orders = new ArrayList<>();
         orders.add(new Order(1,new Point(1,2),new Point(2,2),new Time("12:00","13:00"),10));
         orders.add(new Order(2,new Point(3,2),new Point(3,2),new Time("13:00","14:00"),11));
-        orders.add(new Order(3,new Point(1,7),new Point(3,6),new Time("14:00","15:00"),12));
-        orders.add(new Order(4,new Point(9,7),new Point(8,6),new Time("15:00","16:00"),13));
-        orders.add(new Order(5,new Point(7,1),new Point(1,7),new Time("16:00","17:00"),14));
-        orders.add(new Order(6,new Point(1,4),new Point(9,7),new Time("17:00","18:00"),15));
-        orders.add(new Order(7,new Point(2,6),new Point(4,7),new Time("18:00","19:00"),16));
-        orders.add(new Order(8,new Point(7,7),new Point(3,5),new Time("19:00","20:00"),17));
+        //orders.add(new Order(3,new Point(1,7),new Point(3,6),new Time("14:00","15:00"),12));
+        //orders.add(new Order(8,new Point(7,7),new Point(3,5),new Time("19:00","20:00"),17));
+        //orders.add(new Order(5,new Point(7,1),new Point(1,7),new Time("16:00","17:00"),14));
+        //orders.add(new Order(6,new Point(1,4),new Point(9,7),new Time("17:00","18:00"),15));
+        //orders.add(new Order(7,new Point(2,6),new Point(4,7),new Time("18:00","19:00"),16));
+        //orders.add(new Order(4,new Point(9,7),new Point(8,6),new Time("15:00","16:00"),13));
         List<Person> persons = new ArrayList<>();
         persons.add(new Person(1, "Stepan", 1, 2, new Point(2,2)));
-        persons.add(new Person(2, "Ivan", 2, 3, new Point(3,3)));
-        persons.add(new Person(3, "Misha", 3, 4, new Point(4,4)));
+        //persons.add(new Person(2, "Ivan", 2, 3, new Point(3,3)));
+        //persons.add(new Person(3, "Misha", 3, 4, new Point(4,4)));
         FirstPurpose firstPurpose = new FirstPurpose();
         for (int i = 0; i < firstPurpose.firstPurpose(orders,persons).getAllPurpose().size(); i++) {
             System.out.println(firstPurpose.firstPurpose(orders,persons).getAllPurpose().get(i));
