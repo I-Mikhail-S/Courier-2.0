9c20aea3806361cceb7233262f607005830d9bbf
Stepan <stepantryastsin@mail.ru>
2023-05-27 09:26:45 +0300

    Функция поиска пути для человека

--- src/main/java/org/example/basicAlgorithm/BasicAlgorithm.java
+++ src/main/java/org/example/basicAlgorithm/BasicAlgorithm.java
@@ -5,15 +5,19 @@ import org.example.abstractPerson.Person;
 import org.example.pupose.Purpose;
 import org.example.schedule.Schedule;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 
 public class BasicAlgorithm {
+    private static ArrayList<Integer> random(int size){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i=1; i<size; i++) list.add(i);
+        Collections.shuffle(list);
+        return  list;
+    }
 
     public static Schedule basicAlgorithm (List<Person> personList, List<Order> orderList) {
+
         if (personList == null && orderList == null) {
             throw new RuntimeException("Передан(ы) пустой(ые) лист(ы)!");
         }
@@ -23,9 +27,9 @@ public class BasicAlgorithm {
             List<Order> unUsedOrder = new ArrayList<>(orderList);
             Schedule ideaSchedule = new Schedule();
 
-            for (int k = 0; unUsedOrder.size() != 0; k++) {
+            for (int k = 0,i = 0; unUsedOrder.size() != 0; k++,i++) {
                 if (k == personList.size()) k = 0;
-                Purpose ideaPurpose = new Purpose(personList.get(k), unUsedOrder.get(0));
+                Purpose ideaPurpose = new Purpose(personList.get(k), unUsedOrder.get(random(orderList.size()).get(i)));
                 ideaSchedule.addPurpose(ideaPurpose);
                 unUsedOrder.remove(unUsedOrder.get(0));
             }
